d83d1bca1090a24abea753f8314ab0d7f19e83fd
Josh Morris <jmo0624@gmail.com>
2020-03-10 00:19:17 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -12,7 +12,8 @@ class PetsController < ApplicationController
 
   post '/pets' do 
     @pet = Pet.create(params["pet"])
-    if !params["owner"]["name"]
+    if !params["owner"]["name"].empty?
+      
     redirect to "pets/#{@pet.id}"
   end
 

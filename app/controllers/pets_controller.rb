bf1364e9614a167f3c02d89feb439d734597f3a1
Josh Morris <jmo0624@gmail.com>
2020-03-10 00:20:14 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -14,6 +14,9 @@ class PetsController < ApplicationController
     @pet = Pet.create(params["pet"])
     if !params["owner"]["name"].empty?
       @pet.owner = Owner.create(name: params["owner"]["name"])
+    end
+    @pet.save
+    
     redirect to "pets/#{@pet.id}"
   end
 
